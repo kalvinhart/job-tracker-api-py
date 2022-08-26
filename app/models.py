fe85c0f4f031e0b081a786d225a93c619afa5ce4
Kalvin <kalvinhart@hotmail.co.uk>
2022-08-26 16:39:12 +0100

    created the User model

--- app/models.py
+++ app/models.py
@@ -9,7 +9,17 @@ class Job(db.Model):
     salary = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text, nullable=True)
     date_applied = db.Column(db.DateTime, nullable=False)
+    date_updated = db.Column(db.DateTime, nullable=False)
     interview_date = db.Column(db.DateTime, nullable=True)
     benefits = location = db.Column(db.String(100), nullable=False)
     contact_name = db.Column(db.String(40), nullable=True)
     contact_number = db.Column(db.String(11), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(20), unique=True, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False)
+    last_login = db.Column(db.DateTime, nullable=False)
+    jobs = db.relationship("Job", backref=db.backref("user", lazy="joined"), lazy=True)
